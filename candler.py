4f946f3a154bc9b9bc2850a754391d3b322b40a2
Ryan Francis <2yan@outlook.com>
2017-12-16 10:36:31 -0800

    updates

--- candler.py
+++ candler.py
@@ -10,7 +10,7 @@ class Candler():
     r = None
     url = None
     fire = None
-
+    logger = None
     product_id = None
     last_message = None
     time_difference = None
@@ -19,7 +19,6 @@ class Candler():
     def __init__(self,product_id, candle_width_seconds = 60.0000000001 , url = 'https://api.gdax.com'):
         #Initializes Values
         self.url = url
-
         self.product_id = product_id
         self.last_message = datetime.now()
         self.candle_width_seconds = candle_width_seconds
@@ -29,9 +28,15 @@ class Candler():
         end = datetime.now() - timedelta(seconds = 60 * 60 * 12)
         start = end - timedelta(seconds = 60 * 200)
         candles = self.get_candles( start, end, candle_width_seconds)
-        with sqlite3.connect('fire.db', check_same_thread = False) as con:
-            candles.to_sql(product_id, con, if_exists = 'replace')
-            con.commit()
+        done = False
+        while not done:
+            with sqlite3.connect('fire.db', check_same_thread = False) as con:
+                try:
+                    candles.to_sql(product_id, con, if_exists = 'replace')
+                    con.commit()
+                    done = True
+                except sqlite3.OperationalError:
+                    pass
         
     def __get(self, path, params=None):
         #Requests passthrough, tries to avoid the too many requests code by adding pauses
@@ -39,11 +44,27 @@ class Candler():
         now = datetime.now()
         if (now - self.last_message).total_seconds() <= 1:
             time.sleep(1)
-        r = requests.get(self.url + path, params=params, timeout=30, verify = True)
+        try:
+            r = requests.get(self.url + path, params=params, timeout=30, verify = True)
+        except ConnectionError as e:
+            with open(self.product_id + ' LOG.txt', 'a+') as logfile:
+                message = datetime.now().isoformat() + ' -----\n'
+                message = message + 'CONNECTIONERROR\n'
+                message = message + repr(e)
+                logfile.write(message)
+                raise
+            
         if r.status_code == 429:
             time.sleep(2)
             r = requests.get(self.url + path, params=params, timeout=30, verify = True)
         self.r = r
+        if r.status_code != 200:
+            with open(self.product_id + ' LOG.txt', 'a+') as logfile:
+                message = datetime.now().isoformat()+ ' ---- '
+                message = message + 'staus_code: {}'.format(r.status_code)
+                message = message + 'reason: {}'.format(r.reason)
+                message = message + 'text: {}\n'.format(r.text)
+                logfile.write( message  )
         self.last_message = datetime.now()
         return r.json()
 
@@ -76,10 +97,24 @@ class Candler():
         delete from [{}]
         where time in ('{}')
         """.format( self.product_id, remove_list)
+        
         with sqlite3.connect('fire.db', check_same_thread = False) as con:
-            con.execute(sql)
-            con.commit()
-            candles.to_sql(self.product_id, con, if_exists = 'append')
+            done = False
+            while not done:
+                try:
+                    con.execute(sql)
+                    con.commit()
+                    done = True
+                except sqlite3.OperationalError:
+                    pass
+            done = False
+            while not done:
+                try:
+                    candles.to_sql(self.product_id, con, if_exists = 'append')
+                    done = True
+                except sqlite3.OperationalError:
+                    pass
+                
         return 
 
     
