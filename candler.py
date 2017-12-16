cdf1808903ec7d76b0282c40abe43bb0847bc695
Ryan Francis <2yan@outlook.com>
2017-12-16 11:00:13 -0800

    multiprocessig

--- candler.py
+++ candler.py
@@ -4,13 +4,14 @@ import time
 import pandas as pd
 import requests
 from datetime import datetime, timedelta
+import multiprocessing
 
 class Candler():
     
     r = None
     url = None
     fire = None
-    logger = None
+    logfile = None
     product_id = None
     last_message = None
     time_difference = None
@@ -18,12 +19,13 @@ class Candler():
     
     def __init__(self,product_id, candle_width_seconds = 60.0000000001 , url = 'https://api.gdax.com'):
         #Initializes Values
+        self.logfile = 'logs/' + product_id + '.txt'
         self.url = url
         self.product_id = product_id
         self.last_message = datetime.now()
         self.candle_width_seconds = candle_width_seconds
         self.fire = sql_tools.IcePick(sqlite3, 'fire.db')
-        self.time_difference = pd.to_datetime(self.__get('/time')['iso']) - datetime.now()
+        self.time_difference = pd.to_datetime(self.get('/time')['iso']) - datetime.now()
         
         end = datetime.now() - timedelta(seconds = 60 * 60 * 12)
         start = end - timedelta(seconds = 60 * 200)
@@ -38,28 +40,34 @@ class Candler():
                 except sqlite3.OperationalError:
                     pass
         
-    def __get(self, path, params=None):
+    def get(self, path, params=None):
         #Requests passthrough, tries to avoid the too many requests code by adding pauses
         self.last_message
         now = datetime.now()
         if (now - self.last_message).total_seconds() <= 1:
             time.sleep(1)
         try:
-            r = requests.get(self.url + path, params=params, timeout=30, verify = True)
+            done = False
+            while not done:
+                r = requests.get(self.url + path, params=params, timeout=30, verify = True)
+                if r.status_code == 429:
+                    time.sleep(2)
+                    pass
+                if r.status_code != 429:
+                    done = True
+                    
         except ConnectionError as e:
-            with open(self.product_id + ' LOG.txt', 'a+') as logfile:
+            with open(self.logfile, 'a+') as logfile:
                 message = datetime.now().isoformat() + ' -----\n'
                 message = message + 'CONNECTIONERROR\n'
                 message = message + repr(e)
                 logfile.write(message)
                 raise
             
-        if r.status_code == 429:
-            time.sleep(2)
-            r = requests.get(self.url + path, params=params, timeout=30, verify = True)
+
         self.r = r
         if r.status_code != 200:
-            with open(self.product_id + ' LOG.txt', 'a+') as logfile:
+            with open( self.logfile, 'a+') as logfile:
                 message = datetime.now().isoformat()+ ' ---- '
                 message = message + 'staus_code: {}'.format(r.status_code)
                 message = message + 'reason: {}'.format(r.reason)
@@ -79,7 +87,7 @@ class Candler():
             params['end'] = end.isoformat()
         if granularity is not None:
             params['granularity'] = granularity
-        candles = self.__get('/products/{}/candles'.format(str(self.product_id)), params=params)
+        candles = self.get('/products/{}/candles'.format(str(self.product_id)), params=params)
         candles = pd.DataFrame(candles, columns = [ 'time', 'low', 'high', 'open', 'close', 'volume' ])
         candles['time_string'] = candles['time'].apply(datetime.fromtimestamp)
         candles.sort_values('time', inplace= True)
@@ -136,3 +144,22 @@ class Candler():
                 update_candles()
                 current_minute = now
 
+ids = ['LTC-EUR',
+ 'LTC-USD',
+ 'LTC-BTC',
+ 'ETH-EUR',
+ 'ETH-USD',
+ 'ETH-BTC',
+ 'BTC-GBP',
+ 'BTC-EUR',
+ 'BTC-USD']
+
+def doit(iden):
+    candler = Candler(iden)
+    candler.tuner()
+    
+processes = {}
+    
+for iden in ids:  
+    processes[iden] = multiprocessing.Process(target = doit, args =(iden,))
+    processes[iden].start()
