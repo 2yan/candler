22a08a3b536457c0d6bc1913b820b221ce3fe0cc
Ryan Francis <2yan@outlook.com>
2017-12-17 16:06:42 -0800

    Update Time: 04:06PM on December 17, 2017

--- updater.py
+++ updater.py
@@ -4,7 +4,7 @@ import datetime
 time = datetime.datetime.now().strftime("%I:%M%p on %B %d, %Y")
 
 os.system('git pull')
-os.system('git add fire.db')
+os.system('git add .')
 os.system("git commit -m'Update Time: {}'".format(time))
 os.system('git push')
 
